13e11adcb026357c304695886e158635fc772cd8
RashmiNidhi01 <70425602+RashmiNidhi01@users.noreply.github.com>
2020-09-16 16:16:17 +0530

    Delete solution 2.1.py

--- solution 2.1.py
+++
@@ -1,5 +0,0 @@
-F1=(9/5)*(-40)+32 #formula for converting celsius to Fahrenheit
-F2=(9/5)*(0)+32
-F3=(9/5)*(40)+32
-F4=(9/5)*(100)+32
-print('Coverted celsius to Fahrenheit values','\n',F1,'\n',F2,'\n',F3,'\n',F4)# displaying the output using the formula for F1-F4, where '\n' means line break
